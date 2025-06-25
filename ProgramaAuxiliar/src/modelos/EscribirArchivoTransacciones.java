3b4f17e8993573e4f708ea173d2e5386d7e4b20f
HP SUPPORT <adrian_8_1998@hotmail.com>
2025-06-24 21:15:59 -0500

    Clase EscribirArchivoTransacciones agregada

---
+++ ProgramaAuxiliar/src/modelos/EscribirArchivoTransacciones.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelos;
+
+public class EscribirArchivoTransacciones {
+    private int numTelefoNroCuenta;
+    private int monto;
+    private String Descripcion;
+    private String tipoTransaccion;
+
+    public EscribirArchivoTransacciones(int numTelefoNroCuenta, int monto, String Descripcion, String tipoTransaccion) {
+        this.numTelefoNroCuenta = numTelefoNroCuenta;
+        this.monto = monto;
+        this.Descripcion = Descripcion;
+        this.tipoTransaccion = tipoTransaccion;
+    }
+    
+    public void EscribirArchivo(){
+        
+    }
+    
+}
